2525ff9b1776cfe96e516034f20a0e40044ad677
Chirag Manwani <cmkmanwani@gmail.com>
2023-07-10 15:38:51 +0530

    feat: add support for kong 3.x (#15)

--- kong/plugins/config-by-env/utils.lua
+++ kong/plugins/config-by-env/utils.lua
@@ -1,64 +1,58 @@
-local inspect = require "inspect"
 local function replaceStringEnvVariables(s)
-	local result =
-		string.gsub(
-		s,
-		"%%[A-Z_]+%%",
-		function(str)
-            local env_variable = string.sub(str, 2, string.len(str) - 1)
-            local result = os.getenv(env_variable)
-			if result == nil then
-				kong.log.err("Environment variable is not set: " .. env_variable)
-				error("Throwing error since environment variable is not set: " .. env_variable)
-			end
-            kong.log.notice("Interpolating env variable: " .. env_variable)
-            kong.log.notice("Value::" .. inspect(result))
-            result = result:gsub("\\([nt])", {n="\n", t="\t"})
-			kong.log.debug("Result of replaceStringEnvVariables is: ", result)
-            return result
-		end
-	)
-	return result
+    local result = string.gsub(s, "%%[A-Z_]+%%", function(str)
+        local env_variable = string.sub(str, 2, string.len(str) - 1)
+        local result = os.getenv(env_variable)
+        if result == nil then
+            kong.log.err("Environment variable is not set: " .. env_variable)
+            error("Throwing error since environment variable is not set: " .. env_variable)
+        end
+        result = result:gsub("\\([nt])", {
+            n = "\n",
+            t = "\t"
+        })
+        return result
+    end)
+    return result
 end
 
 local function traverseTableAndTransformLeaves(e, transform_function)
-	for k, v in pairs(e) do -- for every element in the table
-		if type(v) == "table" then
-			local success, err = traverseTableAndTransformLeaves(e[k], transform_function)
-			if not success then
-				return false, err
-			end
-		else
-			if type(v) == "string" then
-				local success, val = pcall(transform_function, v)
-				if not success then
-					kong.log.err("Error inside traverseTableAndTransformLeaves: ", val)
-					return false, val
-				else
-					e[k] = val
-				end
-			end
-		end
-	end
-	return true
+    for k, v in pairs(e) do -- for every element in the table
+        if type(v) == "table" then
+            local success, err = traverseTableAndTransformLeaves(e[k], transform_function)
+            if not success then
+                return false, err
+            end
+        else
+            if type(v) == "string" then
+                local success, val = pcall(transform_function, v)
+                if not success then
+                    kong.log.err("Error inside traverseTableAndTransformLeaves: ", val)
+                    return false, val
+                else
+                    e[k] = val
+                end
+            end
+        end
+    end
+    return true
 end
 
 local function tableMerge(t1, t2)
     if not t2 then
         return t1
     end
-	for k, v in pairs(t2) do
-		if type(v) == "table" then
-			if type(t1[k] or false) == "table" then
-				tableMerge(t1[k] or {}, t2[k] or {})
-			else
-				t1[k] = v
-			end
-		else
-			t1[k] = v
-		end
-	end
-	return t1
+    for k, v in pairs(t2) do
+        if type(v) == "table" then
+            if type(t1[k] or false) == "table" then
+                tableMerge(t1[k] or {}, t2[k] or {})
+            else
+                t1[k] = v
+            end
+        else
+            t1[k] = v
+        end
+    end
+    return t1
 end
 
 local _M = {}
--- spec/config-by-env/cache_invalidation_spec.lua
+++ spec/config-by-env/cache_invalidation_spec.lua
@@ -2,33 +2,34 @@ local cjson = require "cjson"
 local helpers = require "spec.helpers"
 local fixtures = require "spec.config-by-env.fixtures"
 
-for _, strategy in helpers.each_strategy() do
+for _, strategy in helpers.each_strategy({"postgres"}) do
     describe("config-by-env plugin [#" .. strategy .. "]", function()
-        local proxy_client;
+        local proxy_client
         local bp
         local db
-        local mock_host = helpers.mock_upstream_host;
+        local mock_host = helpers.mock_upstream_host
         local mock_port = 10001
         local admin_client
         local app_config_plugin
         setup(function()
-            bp, db = helpers.get_db_utils(strategy, {"routes", "services", "plugins"}, {"config-by-env"});
+            bp, db = helpers.get_db_utils(strategy, {"routes", "services", "plugins"}, {"config-by-env"})
 
             assert(bp.routes:insert({
                 hosts = {"test.com"},
                 protocols = {"http"},
-                service = bp.services:insert(
-                    {
-                        protocol = "http",
-                        host = mock_host, -- Just a dummy value. Not honoured
-                        port = mock_port, -- Just a dummy value. Not honoured
-                        name = "test"
-                    })
+                service = bp.services:insert({
+                    protocol = "http",
+                    host = mock_host, -- Just a dummy value. Not honoured
+                    port = mock_port, -- Just a dummy value. Not honoured
+                    name = "test"
+                })
             }))
 
             local input = {
                 default = {
-                    services = {test = mock_host .. ":" .. mock_port},
+                    services = {
+                        test = mock_host .. ":" .. mock_port
+                    },
                     upstream_port = mock_port
                 },
                 staging = {},
@@ -61,12 +62,13 @@ for _, strategy in helpers.each_strategy() do
 
         describe("Cache is invalidated on updating config-by-env", function()
             it("Initially request should be proxied to old port", function()
-                local res = assert(proxy_client:send(
-                                       {
-                        method = "GET",
-                        path = "/test",
-                        headers = {Host = "test.com"}
-                    }))
+                local res = assert(proxy_client:send({
+                    method = "GET",
+                    path = "/test",
+                    headers = {
+                        Host = "test.com"
+                    }
+                }))
 
                 assert(res.status == 200)
                 local body_data = assert(res:read_body())
@@ -77,7 +79,9 @@ for _, strategy in helpers.each_strategy() do
 
                 local input1 = {
                     default = {
-                        services = {test = mock_host .. ":" .. 10002}, -- this is a new service url
+                        services = {
+                            test = mock_host .. ":" .. 10002
+                        }, -- this is a new service url
                         upstream_port = mock_port
                     },
                     staging = {},
@@ -86,22 +90,28 @@ for _, strategy in helpers.each_strategy() do
 
                 local url = "/plugins/" .. app_config_plugin["id"]
 
-                local admin_res = assert(
-                                      admin_client:patch(url, {
-                        headers = {["Content-Type"] = "application/json"},
-                        body = {
-                            name = "config-by-env",
-                            config = {config = cjson.encode(input1)}
+                local admin_res = assert(admin_client:patch(url, {
+                    headers = {
+                        ["Content-Type"] = "application/json"
+                    },
+                    body = {
+                        name = "config-by-env",
+                        config = {
+                            config = cjson.encode(input1)
                         }
-                    }))
+                    }
+                }))
                 assert.res_status(200, admin_res)
 
-                local res1 = assert(proxy_client:send(
-                                        {
-                        method = "GET",
-                        path = "/test",
-                        headers = {Host = "test.com"}
-                    }))
+                helpers.wait_for_invalidation("config-by-env-final")
+
+                local res1 = assert(proxy_client:send({
+                    method = "GET",
+                    path = "/test",
+                    headers = {
+                        Host = "test.com"
+                    }
+                }))
 
                 assert(res1.status == 200)
                 local body_data1 = assert(res1:read_body())
